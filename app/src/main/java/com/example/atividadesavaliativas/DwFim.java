def3071e1370a06c2eb3955823b01f452d68da8d
Alexandre de Jesus <alexandrej.engenharia@gmail.com>
2023-01-07 09:07:18 -0300

    adicionado botões na tela final

--- app/src/main/java/com/example/atividadesavaliativas/DwFim.java
+++ app/src/main/java/com/example/atividadesavaliativas/DwFim.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class DwFim extends AppCompatActivity {
@@ -12,6 +14,7 @@ public class DwFim extends AppCompatActivity {
     TextView acertos,total;
     String mensagem1 = "de ";
     String mensagem2 = " questões";
+    Button sair, reiniciar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,8 @@ public class DwFim extends AppCompatActivity {
         setContentView(R.layout.activity_dw_fim);
         acertos = findViewById(R.id.acertos);
         total = findViewById(R.id.total_questoes);
+        sair = findViewById(R.id.bt_sair);
+        reiniciar = findViewById(R.id.bt_reiniciar);
 
 
         Intent intent = getIntent();
@@ -27,9 +32,27 @@ public class DwFim extends AppCompatActivity {
             Questao = intent.getIntExtra("questaoEscolhida", 0);
 
         }
-        String mensagem = mensagem1+String.valueOf(Placar)+mensagem2;
+        String placar = String.valueOf(Placar);
+        String mensagem = mensagem1+placar+mensagem2;
         acertos.setText(String.valueOf(Placar));
         total.setText(mensagem);
+        sair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finishAffinity();
+
+            }
+
+        });
+        reiniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(DwFim.this,MainActivity.class);
+                startActivity(i);
+
+            }
+        });
 
 
     }
